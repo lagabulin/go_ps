3cd277ab2fc0db87b44440bbffb8506bb67036ac
lagabulin <shelloos59@gmail.com>
2025-12-30 21:31:25 +0900

    1000번 추가

---
+++ 0x01_time/1000/1000.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
+
+var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+    defer writer.Flush()
+
+    var A, B int
+    fmt.Fscan(reader, &A, &B)
+    fmt.Fprintln(writer, A+B)
+}
